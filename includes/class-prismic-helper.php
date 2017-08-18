5d182734c2c226907c7ff312184774953c3baf2a
Simon Larsson <simon@reload.dk>
2017-08-18 11:36:25 +0200

    Checkstyle fix and cleanup - DCH-8

--- includes/class-prismic-helper.php
+++ includes/class-prismic-helper.php
@@ -1,91 +1,116 @@
 <?php
-
-require_once plugin_dir_path( __FILE__ ) . 'class-link-resolver.php';
+/**
+ * @file Prismic helper.
+ */
 
 use Prismic\Api;
 use Prismic\Predicates;
+
+require_once plugin_dir_path( __FILE__ ) . 'class-link-resolver.php';
 
 /**
  * This class contains helpers for the Prismic API.
  */
 class Prismic_Helper {
 
-  public $linkResolver;
-  private $api = null;
+	/**
+	 * Prismic link resolver class.
+	 *
+	 * @var PrismicLinkResolver $link_resolver
+	 */
+	public $link_resolver;
 
-  public function __construct() {
-    $this->linkResolver = new PrismicLinkResolver($this);
+	/**
+	 * Prismic API helper.
+	 *
+	 * @var API $api
+	 */
+	private $api;
 
-    // $url = $container->get('settings')['prismic.url'];
-    // $token = $container->get('settings')['prismic.token'];
-    $url = 'https://dac-content-hub.cdn.prismic.io/api';
-    $token = null;
-    try {
-      $this->api = Api::get($url, $token);
-    } catch (Exeption $e) {
-      echo 'Caught exception: ',  $e->getMessage(), "\n";
-    }
-  }
+	/**
+	 * Constructor.
+	 */
+	public function __construct() {
+		$this->link_resolver = new PrismicLinkResolver( $this );
 
-  /**
-   * Expose api.
-   */
-  public function get_api() {
-    return $this->api;
-  }
+		// $url = $container->get('settings')['prismic.url'];
+		// $token = $container->get('settings')['prismic.token'];
+		$url = 'https://dac-content-hub.cdn.prismic.io/api';
+		$token = null;
+		try {
+			$this->api = Api::get( $url, $token );
+		} catch ( Exeption $e ) {
+			echo 'Caught exception: ',  $e->getMessage(), "\n";
+		}
+	}
 
-  /**
-   * Format range query.
-   */
-  private function rangeQuery($type, $name, $value) {
-    $numbers = preg_match_all('/([\d]+)/', $value, $matches)
-      ? array_map('intval', $matches[1])
-      : NULL;
-    unset($matches);
-    switch (count($numbers)) {
-      case 1:
-        return Predicates::at("my.$type.$name", (int) reset($numbers));
+	/**
+	 * Expose api.
+	 */
+	public function get_api() {
+		return $this->api;
+	}
 
-      case 2:
-        return Predicates::inRange("my.$type.$name", $numbers[0], $numbers[1]);
+	/**
+	 * Format range query.
+	 *
+	 * @param string $type Content type.
+	 * @param string $name Field name.
+	 * @param string $value Field value.
+	 *
+	 * @return array List of query predicates.
+	 */
+	private function range_query( $type, $name, $value ) {
+		$numbers = preg_match_all( '/([\d]+)/', $value, $matches )
+		? array_map( 'intval', $matches[1] )
+		: null;
+		unset( $matches );
+		switch ( count( $numbers ) ) {
+			case 1:
+				return Predicates::at( "my.$type.$name", (int) reset( $numbers ) );
 
-      default:
-        return FALSE;
-    }
-  }
+			case 2:
+				return Predicates::inRange( "my.$type.$name", $numbers[0], $numbers[1] );
 
-  /**
-   * Query builder.
-   */
-  public function query($predicates) {
-    $query = [];
-    $type = $predicates['type'] ?: NULL;
-    foreach ($predicates as $name => $value) {
-      if ($value && in_array($name, ['type', 'id', 'tags'])) {
-        $query[] = Predicates::at("document.$name", $value);
-      }
-      if ($value && $type && !in_array($name, ['type', 'id', 'tags', 'view_mode'])) {
-        switch ($name) {
-          case 'organisation':
-            // We need to use the group this field is in.
-            $query[] = Predicates::at("my.$type.actors.$name", $value);
-            break;
+			default:
+				return false;
+		}
+	}
 
-          case 'build_year':
-            // Call range query builder.
-            $result = $this->rangeQuery($type, $name, $value);
-            if ($result) {
-              $query[] = $result;
-            }
-            break;
+	/**
+	 * Query builder.
+	 *
+	 * @param array $predicates Query predicates.
+	 */
+	public function query( $predicates ) {
+		$query = [];
+		$type = $predicates['type'] ?: null;
+		foreach ( $predicates as $name => $value ) {
+			if ( $value && in_array( $name, [ 'type', 'id', 'tags' ], true ) ) {
+				$query[] = Predicates::at( "document.$name", $value );
+			}
+			if ( $value && $type && ! in_array( $name, [ 'type', 'id', 'tags', 'view_mode' ], true ) ) {
+				switch ( $name ) {
+					case 'organisation':
+						// We need to use the group this field is in.
+						$query[] = Predicates::at( "my.$type.actors.$name", $value );
+						break;
 
-          default:
-            $query[] = Predicates::at("my.$type.$name", $value);
-        }
-      }
-    }
+					case 'build_year':
+						// Call range query builder.
+						$result = $this->range_query( $type, $name, $value );
+						if ( $result ) {
+							$query[] = $result;
+						}
+						break;
 
-    return $this->api->query($query);
-  }
+					default:
+						$query[] = Predicates::at( "my.$type.$name", $value );
+				}
+			}
+		}
+
+		return $this->api->query( $query );
+	}
 
 }
--- public/class-dac-content-hub-public.php
+++ public/class-dac-content-hub-public.php
@@ -1,62 +1,67 @@
 <?php
+/**
+ * The public-facing functionality of the plugin.
+ *
+ *  @link       http://socialsquare.dk
+ *  @since      1.0.0
+ *
+ * @package    Dac_Content_Hub
+ * @subpackage Dac_Content_Hub/public
+ */
 
 /**
-* The public-facing functionality of the plugin.
-*
-* @link       http://socialsquare.dk
-* @since      1.0.0
-*
-* @package    Dac_Content_Hub
-* @subpackage Dac_Content_Hub/public
-*/
-
-/**
-* The public-facing functionality of the plugin.
-*
-* Defines the plugin name, version, and two examples hooks for how to
-* enqueue the admin-specific stylesheet and JavaScript.
-*
-* @package    Dac_Content_Hub
-* @subpackage Dac_Content_Hub/public
-* @author     Kræn Hansen <kraen@socialsquare.dk>
-*/
+ * The public-facing functionality of the plugin.
+ *
+ * Defines the plugin name, version, and two examples hooks for how to
+ * enqueue the admin-specific stylesheet and JavaScript.
+ *
+ * @package    Dac_Content_Hub
+ * @subpackage Dac_Content_Hub/public
+ * @author     Kræn Hansen <kraen@socialsquare.dk>
+ */
 class Dac_Content_Hub_Public {
 
 	/**
-	* The ID of this plugin.
-	*
-	* @since    1.0.0
-	* @access   private
-	* @var      string    $plugin_name    The ID of this plugin.
-	*/
+	 * The ID of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string    $plugin_name    The ID of this plugin.
+	 */
 	private $plugin_name;
 
 	/**
-	* The version of this plugin.
-	*
-	* @since    1.0.0
-	* @access   private
-	* @var      string    $version    The current version of this plugin.
-	*/
+	 * The version of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string    $version    The current version of this plugin.
+	 */
 	private $version;
 
 	/**
-	* The helper used when communicating with the prismic CMS.
-	*
-	* @since    1.0.0
-	* @access   private
-	* @var      Prismic_Helper    $prismic    The prismic helper
-	*/
+	 * The helper used when communicating with the prismic CMS.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      Prismic_Helper    $prismic    The prismic helper
+	 */
 	private $prismic;
+
+	/**
+	 * Base path.
+	 *
+	 * @var string
+	 */
 	private $base_path = 'content';
 
 	/**
-	* Initialize the class and set its properties.
-	*
-	* @since    1.0.0
-	* @param      string    $plugin_name       The name of the plugin.
-	* @param      string    $version    The version of this plugin.
-	*/
+	 * Initialize the class and set its properties.
+	 *
+	 * @since 1.0.0
+	 * @param string $plugin_name The name of the plugin.
+	 * @param string $version The version of this plugin.
+	 */
 	public function __construct( $plugin_name, $version ) {
 
 		$this->plugin_name = $plugin_name;
@@ -67,33 +72,31 @@ class Dac_Content_Hub_Public {
 	}
 
 	/**
-	* Register the stylesheets for the public-facing side of the site.
-	*
-	* @since    1.0.0
-	*/
+	 * Register the stylesheets for the public-facing side of the site.
+	 *
+	 * @since 1.0.0
+	 */
 	public function enqueue_styles() {
 
 		/**
-		* This function is provided for demonstration purposes only.
-		*
-		* An instance of this class should be passed to the run() function
-		* defined in Dac_Content_Hub_Loader as all of the hooks are defined
-		* in that particular class.
-		*
-		* The Dac_Content_Hub_Loader will then create the relationship
-		* between the defined hooks and the functions defined in this
-		* class.
-		*/
-
+		 * This function is provided for demonstration purposes only.
+		 *
+		 * An instance of this class should be passed to the run() function
+		 * defined in Dac_Content_Hub_Loader as all of the hooks are defined
+		 * in that particular class.
+		 *
+		 * The Dac_Content_Hub_Loader will then create the relationship
+		 * between the defined hooks and the functions defined in this
+		 * class.
+		 */
 		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/dac-content-hub-public.css', array(), $this->version, 'all' );
-
 	}
 
 	/**
-	* Register the JavaScript for the public-facing side of the site.
-	*
-	* @since    1.0.0
-	*/
+	 * Register the JavaScript for the public-facing side of the site.
+	 *
+	 * @since    1.0.0
+	 */
 	public function enqueue_scripts() {
 
 		/**
@@ -112,31 +115,47 @@ class Dac_Content_Hub_Public {
 
 	}
 
+	/**
+	 * URL query variables.
+	 *
+	 * @param array $vars Query variables.
+	 *
+	 * @return array $vars
+	 */
 	public function add_query_vars( $vars ) {
-		$vars[] = "content_type";
-		$vars[] = "content_uid";
+		$vars[] = 'content_type';
+		$vars[] = 'content_uid';
 		return $vars;
 	}
 
+	/**
+	 * Add rewrite rules to WordPress.
+	 *
+	 * @return void
+	 */
 	public function add_rewrite_rules() {
 		// @see https://codex.wordpress.org/Rewrite_API/add_rewrite_rule
 		$regex = '^' . $this->base_path . '/([a-z1-9\-_]+)/([a-z1-9\-_]+)/?';
 		$redirect = 'index.php?content_type=$matches[0]&content_uid=$matches[1]';
 		$after = 'top';
-		add_rewrite_rule($regex, $redirect, $after);
+		add_rewrite_rule( $regex, $redirect, $after );
 	}
 
-	public function posts_pre_query($return, WP_Query $query) {
-		$has_content_type = array_key_exists('content_type', $query->query_vars);
-		$has_content_uid = array_key_exists('content_uid', $query->query_vars);
+	/**
+	 * Hijack wordpress queries.
+	 *
+	 */
+	public function posts_pre_query( $return, WP_Query $query ) {
+		$has_content_type = array_key_exists( 'content_type', $query->query_vars );
+		$has_content_uid = array_key_exists( 'content_uid', $query->query_vars );
 
-		if ($has_content_type && $has_content_uid) {
+		if ( $has_content_type && $has_content_uid ) {
 			$content_type = $query->query_vars['content_type'];
 			$content_uid = $query->query_vars['content_uid'];
 
-			$api = $this->prismic->getAapi();
-			$content = $api->getByUID($content_type, $content_uid);
-			$content_object = (object) $this->post_data_from_content($content);
+			$api = $this->prismic->get_api();
+			$content = $api->getByUID( $content_type, $content_uid );
+			$content_object = (object) $this->post_data_from_content( $content );
 
 			$query->queried_object = $content_object;
 			$query->queried_object_id = $content->getUID();
@@ -144,110 +163,111 @@ class Dac_Content_Hub_Public {
 			$query->is_single = true;
 
 			return array(
-				$content_object
+				$content_object,
 			);
 		}
 	}
 
-	private function post_data_from_content($content) {
+	/**
+	 * Hijack a WordPress page.
+	 */
+	private function post_data_from_content( $content ) {
 		$type = $content->getType();
-		$resolver = $this->prismic->linkResolver;
+		$resolver = $this->prismic->link_resolver;
 
-		if($type === 'case') {
+		if ( 'case' === $type ) {
 			$first_publication = $content->getFirstPublicationDate();
 			$last_publication = $content->getLastPublicationDate();
-			$short_description = $content->getStructuredText('case.short-description');
-			// The ID needs to be faked to trick get_metadata into returning metadata
+			$short_description = $content->getStructuredText( 'case.short-description' );
+			// The ID needs to be faked to trick get_metadata into returning metadata.
 			return array(
 				'ID' => PHP_INT_MAX,
 				'post_type' => $type,
-				'post_title' => $content->getText('case.title'),
+				'post_title' => $content->getText( 'case.title' ),
 				'post_name' => $content->getUID(),
-				'post_date' => $first_publication ? $first_publication->format('Y-m-d H:i:s') : null,
-				'post_modified' => $last_publication ? $last_publication->format('Y-m-d H:i:s') : null,
-				'post_excerpt' => $short_description ? $short_description->asHtml($resolver) : '',
-				'post_content' => $content->getStructuredText('case.description')->asHtml($resolver),
-				'content' => $content
+				'post_date' => $first_publication ? $first_publication->format( 'Y-m-d H:i:s' ) : null,
+				'post_modified' => $last_publication ? $last_publication->format( 'Y-m-d H:i:s' ) : null,
+				'post_excerpt' => $short_description ? $short_description->asHtml( $resolver ) : '',
+				'post_content' => $content->getStructuredText( 'case.description' )->asHtml( $resolver ),
+				'content' => $content,
 			);
 		} else {
-			throw new Error('Unexpected content-type: ' . $type);
+			throw new Error( 'Unexpected content-type: ' . $type );
 		}
 	}
 
-	public function get_post_metadata($value, $object_id, $meta_key, $single) {
+	public function get_post_metadata( $value, $object_id, $meta_key, $single ) {
 		$post = get_post();
-		if($meta_key === '_thumbnail_id' && $post->post_type === 'case') {
+		if ( '_thumbnail_id' === $meta_key && 'case' === $post->post_type ) {
 			return true;
 		} else {
 			return $value;
 		}
 	}
 
-	// post_thumbnail_html
-
-	public function post_thumbnail_html($html, $post_id, $post_thumbnail_id, $size, $attr) {
-		$post = get_post();
-		if($post->post_type === 'case') {
-			$pictures = $post->content->getGroup('case.pictures')->getArray();
-		}
+	/**
+	 * Build a content link.
+	 *
+	 * @param string $type Content type.
+	 * @param string $urn  Content url.
+	 */
+	public function generate_content_link( $type, $urn ) {
+		return home_url( '/' . $this->base_path . '/' . $type . '/' . $urn );
 	}
 
-	public function generate_content_link($type, $urn) {
-		return home_url('/' . $this->base_path . '/' . $type . '/' . $urn);
-	}
-
-	public function post_link($permalink, $post, $leavename) {
-		if(property_exists($post, 'content')) {
-			// We assume it's from the content hub
-			return $this->generate_content_link($post->post_type, $post->post_name);
+	/**
+	 * Build post link.
+	 */
+	public function post_link( $permalink, $post, $leavename ) {
+		if ( property_exists( $post, 'content' ) ) {
+			// We assume it's from the content hub.
+			return $this->generate_content_link( $post->post_type, $post->post_name );
 		}
 		return $permalink;
 	}
 
-	public static function overlay_html($inside_html) {
-		return '<div class="dac-collage__overlay">' . $inside_html . '</div>';
-	}
-
 	/**
 	 * Twig powered data formatter.
+	 *
+	 * @param object $doc Prismic document.
 	 */
-	public function dac_format_data($doc) {
+	public function dac_format_data( $doc ) {
 		$type = $doc->getType();
 		// Use twig for templating.
-		$loader = new Twig_Loader_Filesystem(plugin_dir_path( __FILE__ ) . 'templates');
-		$twig = new Twig_Environment($loader);
+		$loader = new Twig_Loader_Filesystem( plugin_dir_path( __FILE__ ) . 'templates' );
+		$twig = new Twig_Environment( $loader );
 		// Prismic link resolver.
-		$resolver = $this->prismic->linkResolver;
+		$resolver = $this->prismic->link_resolver;
 		// Generate url.
-		$href = $this->generate_content_link($type, $doc->getSlug());
+		$href = $this->generate_content_link( $type, $doc->getSlug() );
 		// Decide template per content type.
 		$template = '';
 		$context = [];
-		switch ($type) {
+		switch ( $type ) {
 			case 'case':
-			    // Teaser template file.
+				// Teaser template file.
 				$template = 'case--teaser.html.twig';
 				// Content.
-				$images = $doc->getGroup('case.pictures')->getArray();
-				$image_attributes = array_map(function ($image) {
+				$images = $doc->getGroup( 'case.pictures' )->getArray();
+				$image_attributes = array_map(function ( $image ) {
 					return [
-						'src' => $image->getImage('picture')->getUrl(),
-						'alt' =>  $image->getImage('picture')->getAlt(),
-						'width' => $image->getImage('picture')->getWidth(),
-						'height' => $image->getImage('picture')->getHeight(),
+						'src' => $image->getImage( 'picture' )->getUrl(),
+						'alt' => $image->getImage( 'picture' )->getAlt(),
+						'width' => $image->getImage( 'picture' )->getWidth(),
+						'height' => $image->getImage( 'picture' )->getHeight(),
 					];
 				}, $images);
-				$image_attributes_first = array_shift($image_attributes);;
-			    $context = [
-					'title' => $doc->getText('case.title'),
+				$image_attributes_first = array_shift( $image_attributes );
+				$context = [
+					'title' => $doc->getText( 'case.title' ),
 					'image' => $image_attributes_first,
-					'teaser_text' => $doc->getStructuredText('case.short-description')->asHtml($resolver),
+					'teaser_text' => $doc->getStructuredText( 'case.short-description' )->asHtml( $resolver ),
 					'href' => $href,
 				];
 				break;
 		}
-		if (!empty($template) && !empty($context)) {
-			return $twig->render($template, $context);
+		if ( ! empty( $template ) && ! empty( $context ) ) {
+			return $twig->render( $template, $context );
 		}
 	}
 
@@ -255,13 +275,15 @@ class Dac_Content_Hub_Public {
 	 * Initialize shortcode.
 	 */
 	public function add_shortcodes() {
-		add_shortcode('content-hub', array($this, 'dac_shortcode'));
+		add_shortcode( 'content-hub', array( $this, 'dac_shortcode' ) );
 	}
 
 	/**
 	 * Shortcode callback.
+	 *
+	 * @param array $attributes Shortcode attributes.
 	 */
-	public function dac_shortcode($attributes = [], $content = null ) {
+	public function dac_shortcode( $attributes = [] ) {
 		$attributes = shortcode_atts(
 			array(
 				// Default to case only.
@@ -280,10 +302,10 @@ class Dac_Content_Hub_Public {
 			'content-collage'
 		);
 
-		$response = $this->prismic->query($attributes);
+		$response = $this->prismic->query( $attributes );
 		$result = '';
-		foreach($response->getResults() as $doc) {
-			$result .= $this->dac_format_data($doc);
+		foreach ( $response->getResults() as $doc ) {
+			$result .= $this->dac_format_data( $doc );
 		}
 		return $result;
 	}
