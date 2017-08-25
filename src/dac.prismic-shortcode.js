196906c89bdf3c24ad27a8bb15f53419d54dc161
Simon Larsson <simon@reload.dk>
2017-08-25 13:55:46 +0200

    Keyboard nav - DCH-25

--- src/dac.prismic-shortcode.js
+++ src/dac.prismic-shortcode.js
@@ -1,5 +1,5 @@
 import Api from './PrismicHelper';
-(($) => {
+(() => {
 	// Get settings.
 	const settings = window.dac_vars || null ;
 
@@ -9,16 +9,31 @@ import Api from './PrismicHelper';
 	}
 	// Init Api.
 	const api = new Api;
-	// Autocomplete handler.
+
+	/**
+	 * Autocomplete handler.
+	 *
+	 * @param {string} name Form element name.
+	 * @param {string} query Api query string.
+	 * @param {object} editor The editor instance.
+	 * @param {object} e Caught eventt.
+	 */
+	let isOpen = false;
 	const dac_autocomplete = (name, query, editor, e) => {
+		console.log(e.target);
+		// Mute space and enter.
+		if ([13, 32].indexOf(e.keyCode) >= 0) {
+			return;
+		}
 		// List to populate with results.
 		const itemList = document.createElement('ul');
 		// Our target textfield.
 		const textbox = e.target;
+		textbox.setAttribute('autocomplete', false);
 		// Our result promise.
 		const result = api.fullTextQuery(query, textbox.value);
 		Promise.resolve(result).then(result => {
-			if (result) {
+			if (result && !isOpen) {
 				// Attach result list.
 				itemList.classList.add('dac-autocomplete--list');
 				// Add calculated styles.
@@ -26,26 +41,88 @@ import Api from './PrismicHelper';
 				itemList.style.top = textbox.style.height;
 				itemList.style.width = textbox.style.width;
 				textbox.parentNode.appendChild(itemList);
+				isOpen = true;
 				// Format results.
 				for (let uid in result) {
 					// Create list item.
-					let elem = document.createElement('li');
-					elem.setAttribute('data-uid', uid)
+					let li = document.createElement('li');
+					let a = document.createElement('a');
+					a.setAttribute('data-uid', uid);
+					a.setAttribute('href', '#');
+					a.setAttribute('tabindex', 1);
 					const value = result[uid];
 					const text = document.createTextNode(value);
-					elem.appendChild(text);
-					elem.style.cursor = 'pointer';
-					// Click handler for result.
-					elem.addEventListener('click', (e) => {
+					a.appendChild(text);
+					// Click handler.
+					a.addEventListener('click', (e) => {
+						e.preventDefault();
 						textbox.value = value;
 						itemList.parentNode.removeChild(itemList);
-						// Set to tinyMce paramaters.
+						isOpen = false;
 						const params = editor.windowManager.getParams();
 						params[name] = e.target.dataset.uid;
 						editor.windowManager.setParams(params);
+						textbox.focus();
 					});
-					itemList.appendChild(elem);
+					// Keydown handler.
+					a.addEventListener('keyup', (e) => {
+						switch (e.keyCode) {
+							case 13:
+							case 32:
+								textbox.value = value;
+								itemList.parentNode.removeChild(itemList);
+								isOpen = false;
+								const params = editor.windowManager.getParams();
+								params[name] = e.target.dataset.uid;
+								editor.windowManager.setParams(params);
+								textbox.focus();
+								break;
+							case 27:
+								itemList.parentNode.removeChild(itemList);
+								textbox.focus();
+								break;
+						}
+					});
+
+					// Append items.
+					li.appendChild(a);
+					itemList.appendChild(li);
+					// Attach ket navigation.
+					listKeyNav(itemList, textbox);
 				}
+			}
+		});
+	}
+
+	/**
+	 * List key navigation.
+	 *
+	 * @param {object} list The list dom node.
+	 * @param {object} element The input dom node to attach the list on.
+	 */
+	const listKeyNav = (list, input) => {
+		const first = list.firstChild;
+		const last = list.lastChild;
+		window.addEventListener('keyup', (e) => {
+			switch (e.keyCode) {
+				// Up key.
+				case 38:
+					if (document.activeElement === input || first.firstChild) {
+						last.firstChild.focus();
+					}
+					else if (document.activeElement.parentElement.previousSibling) {
+						document.activeElement.parentElement.previousSibling.focus();
+					}
+					break;
+					// Down key.
+				case 40:
+					if (document.activeElement === input || last.firstChild) {
+						first.firstChild.focus();
+					}
+					else if (document.activeElement.parentElement.nextSibling) {
+						document.activeElement.parentElement.nextSibling.focus();
+					}
+					break;
 			}
 		});
 	}
@@ -81,7 +158,7 @@ import Api from './PrismicHelper';
 								type: 'textbox',
 								name: 'organisation',
 								label: 'Organisation',
-								onkeyup: (e) => {let results;
+								onkeyup: (e) => {
 									// Autocomplete handler.
 									dac_autocomplete('organisation', 'my.organisation.name', editor, e);
 								}
@@ -128,4 +205,4 @@ import Api from './PrismicHelper';
 	});
 	// Init plugin.
 	tinymce.PluginManager.add( 'dac_shortcode', tinymce.plugins.MyButtons );
-})(jQuery);
+})();
